a6c5e30bc60f4023128ab35a11c01a45e5c0275d
Lewis Liu <lewisl9029@users.noreply.github.com>
2020-09-26 20:49:54 -0700

    Move to structured cache instead of building id
    This was responsible for about 30% of time in rules mapping fn.

--- src/useStyles.js
+++ src/useStyles.js
@@ -16,29 +16,60 @@ const supportedPseudoClasses = new Set([
 const cachedRulesInternal = {}
 const insertedRulesInternal = new Set()
 
-const toRules = ({ styles, pseudoClass = '', cachedRules }) => {
+// const measure = (name, fn) => {
+//   window.performance.mark(`${name}_start`)
+//   const result = fn()
+//   window.performance.mark(`${name}_end`)
+//   window.performance.measure(name, `${name}_start`, `${name}_end`)
+//   // window.performance.clearMark(`${name}_start`)
+//   // window.performance.clearMark(`${name}_end`)
+//   return result
+// }
+
+// const summarize = () => performance.getEntriesByType('measure').reduce((durations, measure) => ({...durations, [measure.name]: (durations[measure.name] || 0) + measure.duration}), {})
+
+// window.summarize = summarize
+
+const measure = (name, fn) => fn()
+
+
+const toRules = ({ styles, cachedRules }) => {
+  // const toRules = ({ styles, pseudoClass = '', cachedRules }) => {
   // TODO: experiment with using single rule per styles
   return Object.entries(styles).flatMap(([name, value]) => {
-    if (supportedPseudoClasses.has(name)) {
-      return toRules({ styles: value, pseudoClass: name, cachedRules })
+    // if (supportedPseudoClasses.has(name)) {
+    //   return toRules({ styles: value, pseudoClass: name, cachedRules })
+    // }
+
+    if (cachedRules[name] && cachedRules[name][value]) {
+      return cachedRules[name][value]
     }
 
-    const id = `${name}_${value}_${pseudoClass}`
+    // const id = `${name}_${value}`
+    const id = measure('id', () => `${name}_${value}`)
+    console.log("uncached rule: " + id)
 
-    if (cachedRules[id]) {
-      return cachedRules[id]
-    }
+    // const className = `r_${hash(id)}`
+    const className = measure('hash', () => `r_${hash(id)}`)
+    // const styleName = hyphenate(name)
+    const styleName = measure('hyphenate', () => hyphenate(name))
 
-    const className = `r_${hash(id)}`
-    const styleName = hyphenate(name)
-    const rule = `.${className}${pseudoClass} { ${styleName}: ${withUnit(
+    // const rule = `.${className} { ${styleName}: ${withUnit(
+    //   name,
+    //   value,
+    // )}; }`
+    const rule = measure('withUnit', () => `.${className} { ${styleName}: ${withUnit(
       name,
       value,
-    )}; }`
+    )}; }`)
 
-    cachedRules[id] = { id, className, rule }
+    if (!cachedRules[name]) {
+      cachedRules[name] = {}
+    }
 
-    return cachedRules[id]
+    cachedRules[name][value] = { id, className, rule }
+
+    return cachedRules[name][value]
   })
 }
 
@@ -50,7 +81,11 @@ export const StylesProvider = ({
 }) => {
   const [stylesheet, setStylesheet] = React.useState()
 
-  React.useLayoutEffect(() => {
+  React.useEffect(() => {
+    if (stylesheet) {
+      return;
+    }
+    // console.log('effect')
     const id = `useStylesStylesheet`
     const existingElement = window.document.getElementById(id)
 
@@ -116,15 +151,41 @@ export const useStyles = (styles, dependencies) => {
 
   const { insertRule, toRules } = cache
 
-  const rules = React.useMemo(() => toRules(styles), dependencies)
+  // const rules = React.useMemo(() => toRules(styles), dependencies)
+  // console.log(dependencies)
+  const rules = measure('rules', () => React.useMemo(() => toRules(styles), dependencies))
 
-  const classNames = React.useMemo(
-    () => rules.map(({ className }) => className).join(' '),
+  const classNames = measure('classNames', () => React.useMemo(
+    // () => rules.map(({ className }) => className).join(' '),
+    () => {
+      const length = rules.length
+      let classNames = ''
+      for(let index = 0; index < length; index++) {
+        classNames+=rules[index].className + ' '
+      }
+      return classNames
+    },
     [rules],
-  )
+  ))
+  // const classNames = React.useMemo(
+  //   // () => rules.map(({ className }) => className).join(' '),
+  //   () => {
+  //     const length = rules.length
+  //     let classNames = ''
+  //     for(let index = 0; index < length; index++) {
+  //       classNames+=rules[index].className + ' '
+  //     }
+  //     return classNames
+  //   },
+  //   [rules],
+  // )
+
 
   React.useLayoutEffect(() => {
-    rules.forEach(insertRule)
+    measure('insert', () => {
+      rules.forEach(insertRule)
+    })
+    // rules.forEach(insertRule)
 
     return () => {
       // This is not necessary, and hinders performance
