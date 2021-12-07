6cc2d9fb2019bc0f370a1574ed711eeb2a4d4564
Lewis Liu <lewisl9029@users.noreply.github.com>
2021-12-06 19:12:38 -0800

    Try unitize with object instead of set

--- src/internal/unitize.js
+++ src/internal/unitize.js
@@ -1,64 +1,64 @@
-const unitlessStyles = new Set([
-  'animationIterationCount',
-  'borderImageOutset',
-  'borderImageSlice',
-  'borderImageWidth',
-  'boxFlex',
-  'boxFlexGroup',
-  'boxOrdinalGroup',
-  'columnCount',
-  'columns',
-  'flex',
-  'flexGrow',
-  'flexPositive',
-  'flexShrink',
-  'flexNegative',
-  'flexOrder',
-  'gridRow',
-  'gridRowEnd',
-  'gridRowSpan',
-  'gridRowStart',
-  'gridColumn',
-  'gridColumnEnd',
-  'gridColumnSpan',
-  'gridColumnStart',
-  'msGridRow',
-  'msGridRowSpan',
-  'msGridColumn',
-  'msGridColumnSpan',
-  'fontWeight',
-  'lineHeight',
-  'opacity',
-  'order',
-  'orphans',
-  'tabSize',
-  'widows',
-  'zIndex',
-  'zoom',
-  'WebkitLineClamp',
+let unitlessKeys = {
+  animationIterationCount: 1,
+  borderImageOutset: 1,
+  borderImageSlice: 1,
+  borderImageWidth: 1,
+  boxFlex: 1,
+  boxFlexGroup: 1,
+  boxOrdinalGroup: 1,
+  columnCount: 1,
+  columns: 1,
+  flex: 1,
+  flexGrow: 1,
+  flexPositive: 1,
+  flexShrink: 1,
+  flexNegative: 1,
+  flexOrder: 1,
+  gridRow: 1,
+  gridRowEnd: 1,
+  gridRowSpan: 1,
+  gridRowStart: 1,
+  gridColumn: 1,
+  gridColumnEnd: 1,
+  gridColumnSpan: 1,
+  gridColumnStart: 1,
+  msGridRow: 1,
+  msGridRowSpan: 1,
+  msGridColumn: 1,
+  msGridColumnSpan: 1,
+  fontWeight: 1,
+  lineHeight: 1,
+  opacity: 1,
+  order: 1,
+  orphans: 1,
+  tabSize: 1,
+  widows: 1,
+  zIndex: 1,
+  zoom: 1,
+  WebkitLineClamp: 1,
 
   // SVG-related properties
-  'fillOpacity',
-  'floodOpacity',
-  'stopOpacity',
-  'strokeDasharray',
-  'strokeDashoffset',
-  'strokeMiterlimit',
-  'strokeOpacity',
-  'strokeWidth',
-])
+  fillOpacity: 1,
+  floodOpacity: 1,
+  stopOpacity: 1,
+  strokeDasharray: 1,
+  strokeDashoffset: 1,
+  strokeMiterlimit: 1,
+  strokeOpacity: 1,
+  strokeWidth: 1
+};
 
 // Taken from https://github.com/facebook/react/blob/b87aabdfe1b7461e7331abb3601d9e6bb27544bc/packages/react-dom/src/shared/dangerousStyleValue.js
 export const unitize = (name, value) => {
-  if (value == null || typeof value === 'boolean' || value === '') {
-    return ''
+  if (value == null || typeof value === "boolean" || value === "") {
+    return "";
   }
 
-  if (typeof value === 'number' && value !== 0 && !unitlessStyles.has(name)) {
-    return `${value}px` // Presumes implicit 'px' suffix for unitless numbers
+  if (typeof value === "number" && value !== 0 && !unitlessKeys[name]) {
+    return `${value}px`; // Presumes implicit 'px' suffix for unitless numbers
   }
 
-  return String(value).trim()
-}
+  return String(value).trim();
+};
 
-export default unitize
+export default unitize;
