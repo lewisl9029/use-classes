398f608b3fed7c31504e729fab2cac9864590fac
Lewis Liu <lewisl9029@users.noreply.github.com>
2021-12-06 19:08:33 -0800

    Update hyphenate function

--- src/internal/hyphenate.js
+++ src/internal/hyphenate.js
@@ -1,30 +1,31 @@
 /**
  * inlined version of
- * https://github.com/facebook/fbjs/blob/master/packages/fbjs/src/core/hyphenate.js
+ * https://github.com/facebook/fbjs/blob/master/packages/fbjs/src/core/hyphenateStyleName.js
+ *
+ * Taken from https://github.com/styled-components/styled-components/blob/30dab74acedfd26d227eebccdcd18c92a1b3bd9b/packages/styled-components/src/utils/hyphenateStyleName.ts
  */
+const uppercaseCheck = /([A-Z])/;
 const uppercasePattern = /([A-Z])/g;
 const msPattern = /^ms-/;
+const prefixAndLowerCase = char => `-${char.toLowerCase()}`;
 
 /**
  * Hyphenates a camelcased CSS property name, for example:
  *
- *   > hyphenate('backgroundColor')
+ *   > hyphenateStyleName('backgroundColor')
  *   < "background-color"
- *   > hyphenate('MozTransition')
+ *   > hyphenateStyleName('MozTransition')
  *   < "-moz-transition"
- *   > hyphenate('msTransition')
+ *   > hyphenateStyleName('msTransition')
  *   < "-ms-transition"
  *
  * As Modernizr suggests (http://modernizr.com/docs/#prefixed), an `ms` prefix
  * is converted to `-ms-`.
- *
- * @param {string} string
- * @return {string}
  */
-export const hyphenate = string =>
-  string
-    .replace(uppercasePattern, "-$1")
-    .toLowerCase()
-    .replace(msPattern, "-ms-");
-
-export default hyphenate;
+export default function hyphenateStyleName(string) {
+  return uppercaseCheck.test(string)
+    ? string
+        .replace(uppercasePattern, prefixAndLowerCase)
+        .replace(msPattern, "-ms-")
+    : string;
+}
